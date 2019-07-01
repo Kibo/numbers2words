ad26575f35991994d5a25da269184a87530c6dda
gcoro <giulia994@ymail.com>
2019-07-01 10:29:36 +0200

    Added build files && Updated README

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ You can use it as [standalone js library](https://github.com/Kibo/numbers2words/
 
 ## Example
 ```
+var T2W = require('numbers2words'); // import from node modules
+
 var translator = new T2W("EN_US");
 // one thousand two hundred thirty-four
 translator.toWords(1234)
@@ -13,6 +15,7 @@ translator.toWords(1234)
 ## Now available locales
  - id_AR (0...999999999) in progress
  - id_ID (0...999999999)
+ - it_IT (0...999999999)
  - de_DE	(0...999999999)
  - en_US	(0...999999999)
  - cs_CZ	(0...999999999)
--- build/numbers2words.js
+++ build/numbers2words.js
@@ -131,6 +131,169 @@ T2W.prototype.tokenize = function( number, tokenLength ){
 };
 
 
+
+/**
+ * ar_AR locale
+ * @constructor
+ */
+T2W.AR_AR = function(){};
+
+/**
+ * Translator dictionary
+ * @constant
+ * @type {Object}
+ */
+T2W.AR_AR.DICTIONARY = {
+	zero		:"صفر",
+	ones		:[ "", "واحد", "اثنان", "ثلاثة", "أربعة", "خمسة", "ستة", "سبعة", "ثمانية", "تسعة" ],
+	teens		:[ "عشرة", "أحد عشر", "اثنا عشر", "ثلاثة عشر", "أربعة عشر", "خمسة عشر", "ستة عشر", "سبعة عشر", "ثمانية عشر", "تسعة عشر" ],
+	tens		:[ "", "", "عشرون", "ثلاثون", "أربعون", "خمسون", "ستة وعشرون", "سبعة وعشرون", "ثمانية وعشرون", "تسعة وعشرون" ],
+	hundred		:"مائة",
+	radix		:["", "ألف", "مليون"],
+	delimiters	:["-", "و"]
+};
+
+/**
+ * Token length
+ * @constant
+ * @type {number}
+ */
+T2W.AR_AR.TOKEN_LENGTH = 3;
+
+/**
+ * Max numbers for this locale
+ * @constant
+ * @type {number}
+ */
+T2W.AR_AR.MAX_NUMBERS = 9;
+
+/**
+ * Translate numbers to words
+ * @public
+ * @param {array} numbers
+ * @param {number} index
+ * @return {string}
+ */
+T2W.AR_AR.prototype.translate = function( numbers ) {	
+	
+	// Check max value	
+	if(numbers.length * T2W.AR_AR.TOKEN_LENGTH > T2W.AR_AR.MAX_NUMBERS){
+		throw {
+			name : "Error",
+			message : "The length of numbers is longer than the maximum value(" + T2W.AR_AR.MAX_NUMBERS + ")."		
+		};	
+	}		
+	
+	// Deal with zero value	
+	if(numbers[T2W.SINGLE_INDEX] === 0 && numbers.length === 1){
+		return T2W.AR_AR.DICTIONARY.zero;
+	}
+	
+	var words = [];
+	for(var idx = 0, max = numbers.length; idx < max; idx++){				
+		words.unshift( this._getTrio( this.tokenize( numbers[idx], 1 ), idx, max));	
+	}
+	
+	return words.join("");								
+};
+
+/**
+ * Converts first three numbers to words.
+ * @private
+ * It solves exceptions in the English language.
+ * @param {Array} numbers
+ * @param {number} index
+ * @param {number} max - length of tokens
+ * @return {string}
+ */
+T2W.AR_AR.prototype._getTrio = function( numbers, index, max){																				
+	var hundred = '';
+	var ten = '';
+	var single = '';
+	var radix = this._getRadix(numbers, index);
+	
+	if(numbers[T2W.HUNDRED_INDEX]){
+		hundred = numbers[T2W.TEN_INDEX] || numbers[T2W.SINGLE_INDEX] 
+			? this._getOnes( numbers[ T2W.HUNDRED_INDEX ] ) + " " + T2W.EN_US.DICTIONARY.hundred + ' ' + T2W.EN_US.DICTIONARY.delimiters[1] + ' '
+			: this._getOnes( numbers[ T2W.HUNDRED_INDEX ] ) + " " + T2W.EN_US.DICTIONARY.hundred;
+	}
+	
+	if( numbers[ T2W.TEN_INDEX ] ){			
+		ten = this._getTeens( numbers[T2W.SINGLE_INDEX]);			
+	}
+						
+	if( numbers[ T2W.TEN_INDEX ] >=2 ){
+		ten = numbers[T2W.SINGLE_INDEX] 
+			? this._getTens( numbers[T2W.TEN_INDEX]) + T2W.EN_US.DICTIONARY.delimiters[0] + this._getOnes( numbers[T2W.SINGLE_INDEX]) 
+			: this._getTens( numbers[T2W.TEN_INDEX]); 	
+	}
+							
+	if( !numbers[ T2W.TEN_INDEX ] ){
+		single = this._getOnes( numbers[T2W.SINGLE_INDEX]);
+	}
+				
+	if(index+1 < max && (numbers[T2W.HUNDRED_INDEX] || numbers[T2W.TEN_INDEX] || numbers[T2W.SINGLE_INDEX]) ){
+		hundred = ' ' + hundred;
+	}
+	
+	if( index === 0 && index+1 < max && !numbers[ T2W.HUNDRED_INDEX ] && (numbers[T2W.TEN_INDEX] || numbers[T2W.SINGLE_INDEX] )){
+		hundred	 = ' ' + T2W.AR_AR.DICTIONARY.delimiters[1] + ' ';		
+	}
+								
+	return hundred + ten + single + radix;		
+};
+
+/**
+ * Get ones
+ * helper method to access the dictionary
+ * @private
+ * @param {number} number
+ * @param {number} index
+ * @return {string}
+ */
+T2W.AR_AR.prototype._getOnes = function( number) {			
+	return T2W.AR_AR.DICTIONARY.ones[ number ];			
+};
+
+/**
+ * Get tens
+ * helper method to access the dictionary
+ * @private
+ * @param {number} number
+ * @return {string}
+ */
+T2W.AR_AR.prototype._getTens = function( number ) {		
+	return T2W.AR_AR.DICTIONARY.tens[ number ];				
+};
+
+/**
+ * Get teens
+ * helper method to access the dictionary
+ * @private
+ * @param {number} number
+ * @return {string}
+ */
+T2W.AR_AR.prototype._getTeens = function(number ){
+	return T2W.AR_AR.DICTIONARY.teens[ number ];
+};
+
+/**
+ * Get radix
+ * convert radix to words
+ * @private
+ * @param {Array} numbers
+ * @param {number} index
+ * @return {string}
+ */
+T2W.AR_AR.prototype._getRadix = function( numbers, index ) {		
+	var radix = '';
+	if( index > 0 && (numbers[T2W.HUNDRED_INDEX] || numbers[T2W.TEN_INDEX] || numbers[T2W.SINGLE_INDEX])){	
+		radix = ' ' + T2W.AR_AR.DICTIONARY.radix[ index ];			
+	}
+			
+	return radix;
+};
+
 /**
  * cs_CZ locale
  * @constructor
@@ -818,6 +981,200 @@ T2W.ID_ID.prototype._getRadix = function( numbers, index ) {
 			
 	return radix;
 };
+/**
+ * it_IT locale
+ * @constructor
+ */
+T2W.IT_IT = function () { };
+
+/**
+ * Translator dictionary
+ * @constant
+ * @type {Object}
+ */
+T2W.IT_IT.DICTIONARY = {
+    zero: "zero",
+    ones: ["", ["uno", "", "un"], "due", "tre", "quattro", "cinque", "sei", "sette", "otto", "nove"],
+    teens: ["dieci", "undici", "dodici", "tredici", "quattordici", "quindici", "sedici", "diciassette", "diciotto", "diciannove"],
+    tens: ["", "", ["venti", "vent"], ["trenta", "trent"], ["quaranta", "quarant"], ["cinquanta", "cinquant"], ["sessanta", "sessant"], ["settanta", "settant"], ["ottanta", "ottant"], ["novanta", "novant"]],
+    hundred: "cento",
+    radix: ["", ["mille", "mila"], ["milione", "milioni"]]
+};
+
+/**
+ * Token length
+ * @constant
+ * @type {number}
+ */
+T2W.IT_IT.TOKEN_LENGTH = 3;
+
+/**
+ * Max numbers for this locale
+ * @constant
+ * @type {number}
+ */
+T2W.IT_IT.MAX_NUMBERS = 9;
+
+/**
+ * Translate numbers to words
+ * @public
+ * @param {array} numbers
+ * @param {number} index
+ * @return {string}
+ */
+T2W.IT_IT.prototype.translate = function (numbers) {
+
+    // Check max value
+    if (numbers.length * T2W.IT_IT.TOKEN_LENGTH > T2W.IT_IT.MAX_NUMBERS) {
+        throw {
+            name: "Error",
+            message: "The length of numbers is longer than the maximum value(" + T2W.IT_IT.MAX_NUMBERS + ")."
+        };
+    }
+
+    // Deal with zero value
+    if (numbers[T2W.SINGLE_INDEX] === 0 && numbers.length === 1) {
+        return T2W.IT_IT.DICTIONARY.zero;
+    }
+
+    var words = [];
+    for (var idx = 0, max = numbers.length; idx < max; idx++) {
+        words.unshift(this._getTrio(this.tokenize(numbers[idx], 1), idx, max, words));
+    }
+
+    return words.join("");
+};
+
+/**
+ * Converts first three numbers to words.
+ * @private
+ * @param {Array} numbers
+ * @param {number} index
+ * @param {number} max - length of tokens
+ * @return {string}
+ */
+T2W.IT_IT.prototype._getTrio = function (numbers, index, max, formerWords) {
+    var hundred = "";
+    var ten = "";
+    var single = "";
+    var radix = this._getRadix(numbers, index);
+    var result = "";
+
+    if (numbers[T2W.HUNDRED_INDEX]) {
+        hundred = this._getOnes(numbers[T2W.HUNDRED_INDEX], index, numbers[T2W.TEN_INDEX], false) + T2W.IT_IT.DICTIONARY.hundred;
+    }
+
+    if (numbers[T2W.TEN_INDEX]) {
+        ten = this._getTeens(numbers[T2W.SINGLE_INDEX]);
+    }
+
+    if (numbers[T2W.TEN_INDEX] >= 2) {
+        ten = numbers[T2W.SINGLE_INDEX]
+            ? this._getTens(numbers[T2W.TEN_INDEX], numbers[T2W.SINGLE_INDEX]) + this._getOnes(numbers[T2W.SINGLE_INDEX])
+            : this._getTens(numbers[T2W.TEN_INDEX]);
+    }
+
+    if (!numbers[T2W.TEN_INDEX]) {
+        single = this._getOnes(numbers[T2W.SINGLE_INDEX], index, numbers[T2W.TEN_INDEX], true);
+    }
+
+    if (index >= 2) {
+        single += " ";
+    }
+
+    result = hundred + ten + single + radix;
+
+    if (index > 1 && formerWords.join("").length > 0) {
+        result += " e ";
+    }
+
+    return result;
+};
+
+/**
+ * Get ones
+ * helper method to access the dictionary
+ * @private
+ * It solves exceptions in the Italian language.
+ * @param {number} number
+ * @param {number} index
+ * @param {boolean} isSingle
+ * @return {string}
+ */
+T2W.IT_IT.prototype._getOnes = function (number, index, tensOfNumber, isSingle) {
+    if (number === 1) {
+        if (index === 0 && isSingle) {
+            return T2W.IT_IT.DICTIONARY.ones[number][0];
+        } else if (index === 0) {
+            return T2W.IT_IT.DICTIONARY.ones[number][1];
+        }
+
+        if (index === 1) {
+            return T2W.IT_IT.DICTIONARY.ones[number][1];
+        }
+
+        if (index >= 2 && isSingle && tensOfNumber === undefined) {
+            return T2W.IT_IT.DICTIONARY.ones[number][2];
+        }
+
+        return T2W.IT_IT.DICTIONARY.ones[number][0];
+    }
+    return T2W.IT_IT.DICTIONARY.ones[number];
+};
+
+/**
+ * Get tens
+ * helper method to access the dictionary
+ * @private
+ * @param {number} number
+ * @return {string}
+ */
+T2W.IT_IT.prototype._getTens = function (number, unit) {
+    if (unit === 1 || unit == 8) {
+        return T2W.IT_IT.DICTIONARY.tens[number][1];
+    }
+    return T2W.IT_IT.DICTIONARY.tens[number][0];
+};
+
+/**
+ * Get teens
+ * helper method to access the dictionary
+ * @private
+ * @param {number} number
+ * @return {string}
+ */
+T2W.IT_IT.prototype._getTeens = function (number) {
+    return T2W.IT_IT.DICTIONARY.teens[number];
+};
+
+/**
+ * Get radix
+ * convert radix to words
+ * @private
+ * @param {Array} numbers
+ * @param {number} index
+ * @return {string}
+ */
+T2W.IT_IT.prototype._getRadix = function (numbers, index) {
+    var radix = "";
+
+    if (index > 0 && (numbers[T2W.HUNDRED_INDEX] || numbers[T2W.TEN_INDEX] || numbers[T2W.SINGLE_INDEX])) {
+        if (index === 1 && numbers.length === 1 && numbers[T2W.SINGLE_INDEX] === 1) {
+            radix = T2W.IT_IT.DICTIONARY.radix[index][0];
+        } else if (index === 1) {
+            radix = T2W.IT_IT.DICTIONARY.radix[index][1];
+        }
+
+        if (index > 1 && numbers.length === 1 && numbers[T2W.SINGLE_INDEX] === 1) {
+            radix = T2W.IT_IT.DICTIONARY.radix[index][0];
+        } else if (index > 1) {
+            radix = T2W.IT_IT.DICTIONARY.radix[index][1];
+        }
+    }
+
+    return radix;
+};
+
 // Node exports
 if(typeof module !== 'undefined' && module.exports){
 	module.exports = T2W;
